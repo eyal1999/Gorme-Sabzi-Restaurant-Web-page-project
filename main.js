3e8be6523c0531553fd53f7d75cc3cc26e7c115e
eyal1999 <eyal1200000@gmail.com>
2024-11-12 09:32:19 +0200

    started about page development (not finished) and content change to swiper slider in index.html file

--- main.js
+++ main.js
@@ -7,11 +7,14 @@ menuBtn.addEventListener('click', e => {
   navLinks.classList.toggle('open');
 
   const isOpen = navLinks.classList.contains('open');
-  menuBtnIcon.setAttribute('class', isOpen ? 'ri-close-line' : 'ri-menu-line');
+  menuBtnIcon.setAttribute(
+    'class',
+    isOpen ? 'fa-solid fa-bars' : 'fa-solid fa-bars'
+  );
 });
 navLinks.addEventListener('click', e => {
   navLinks.classList.remove('open');
-  menuBtnIcon.setAttribute('class', 'ri-menu-line');
+  menuBtnIcon.setAttribute('class', 'fa-solid fa-bars');
 });
 
 const scrollRevealOption = {
